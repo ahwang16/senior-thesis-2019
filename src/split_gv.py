66a32913a626b0b97dc046cf26b1257e8f6bcf54
alyssahwang <ahh2143@columbia.edu>
2020-05-10 00:05:38 -0400

    fix kwargs

--- src/split_gv.py
+++ src/split_gv.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
 		print(x)
 		with open("../data/aae_vocab_{}.pkl".format(x), "rb") as infile:
 			aae_vocab = pkl.load(infile)
-		p = Process(target=eval.kmeans, args=(aae_vocab, "aae", "glove", x))
+		p = Process(target=eval.kmeans, args=(aae_vocab, "aae", "glove",), kwargs={'file_num':x})
 		print("starting", x)
 		p.start()
