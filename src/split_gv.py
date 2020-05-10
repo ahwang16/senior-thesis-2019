981172fa43838d583947b1a00aae4adfcd4f056f
alyssahwang <ahh2143@columbia.edu>
2020-05-09 23:07:41 -0400

    multithreading for aae...

--- src/split_gv.py
+++ src/split_gv.py
@@ -7,7 +7,7 @@ from multiprocessing import Process
 if __name__ == "__main__":
 	for x in range(55):
 		print(x)
-		with open("aae_vocab_{}.pkl".format(x), "rb") as infile:
+		with open("../data/aae_vocab_{}.pkl".format(x), "rb") as infile:
 			aae_vocab = pkl.load(infile)
 		p = Process(target=eval.kmeans, args=(aae_vocab, "aae", "glove", x))
 		print("starting", x)
